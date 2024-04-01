854630aa3749fa9369747f0792ee52513fb4a0e3
ChaozMc <chaozmc@is-jo.org>
2024-04-01 10:43:47 +0200

    initial commit

---
+++ SignAndVerify_withCertificates/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Text;
+using static SignAndVerify_withCertificates.securityOperations;
+
+namespace SignAndVerify_withCertificates
+{
+    internal class securityOperations
+    {
+
+        public string generateSignatureB64(string messageToSign, X509Certificate2 certificate)
+        {
+            try
+            {
+                using (RSA rsa = certificate.GetRSAPrivateKey())
+                {
+                    return Convert.ToBase64String(rsa.SignData(Encoding.UTF8.GetBytes(messageToSign), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+
+        public bool verifySignature(string messageToVerify, string b64Signature, X509Certificate2 certificate)
+        {
+            try
+            {
+                using (RSA rsa = certificate.GetRSAPublicKey())
+                {
+                    return rsa.VerifyData(Encoding.UTF8.GetBytes(messageToVerify), Convert.FromBase64String(b64Signature), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex.InnerException;
+            }
+           
+            
+        }
+
+
+
+            public byte[] GetCertificateFromLocalMachineStore(string friendlyName)
+            {
+                var store = GetLocalMachineCertificates();
+                X509Certificate2 certificate = null;
+                foreach (var cert in store.Cast<X509Certificate2>().Where(cert => cert.FriendlyName.Equals(friendlyName)))
+                {
+                    certificate = cert;
+                }
+                return certificate != null ? certificate.Export(X509ContentType.Pkcs12) : null;
+            }
+
+            private static X509Certificate2Collection GetLocalMachineCertificates()
+            {
+                var localMachineStore = new X509Store(StoreLocation.LocalMachine);
+                localMachineStore.Open(OpenFlags.ReadOnly);
+                var certificates = localMachineStore.Certificates;
+                localMachineStore.Close();
+                return certificates;
+            }
+
+    }
+}
---
+++ SignAndVerify_withCertificates/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SignAndVerify_withCertificates;
+using System.Security.Cryptography.X509Certificates;
+
+securityOperations sc1 = new securityOperations();
+X509Certificate2 myCert = new X509Certificate2(sc1.GetCertificateFromLocalMachineStore("test"));
+
+string signedString = sc1.generateSignatureB64("Hallo", myCert);
+
+Console.WriteLine("The message 'Hallo' signed with the certificate 'test' is:");
+Console.Write(signedString);
+Console.Write('\n');
+Console.Write('\n');
+Console.Write('\n');
+Console.Write('\n');
+Console.WriteLine("The signature is verified: " + sc1.verifySignature("Hallo", signedString, myCert));
+Console.Write('\n');
+Console.Write('\n');
+Console.Write('\n');
+Console.Write('\n');
+
+
+
+
+
+
+
+
+
+
